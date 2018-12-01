09c17b7cbf2ea705859006461b52c861f92b47bc
LUIZ <luizin2@gmail.com>
2018-12-01 15:37:09 -0200

    modificações

--- exemplo-git-eclipse/src/exemplo/Exemplo.java
+++ exemplo-git-eclipse/src/exemplo/Exemplo.java
@@ -5,5 +5,6 @@ public class Exemplo {
 	public static void main(String[] args) {
 		System.out.println("Olá, git");
 		// alterado no github em 01/12/18 as 15:31 hs
+		// alterado no Eclipse em 01/12/18 as 15:35 hs
 	}
 }
